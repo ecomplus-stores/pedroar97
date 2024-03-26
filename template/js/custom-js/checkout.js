bac482f1af1e4a54e7f80ce8e266bf1c73bc33d0
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-03-25 22:29:30 -0300

    chore: recomended items after checkout

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,1 +1,10 @@
 // Add your custom JavaScript for checkout here.
+storefront.on('widget:@ecomplus/widget-tag-manager', () => {
+    setTimeout(() => {
+        if($('#cart').length) {
+          if (window.innerWidth < 767) {
+            $('.cart__discount').after($('.recommended-items'))
+          }
+        }
+    }, 800);
+  });
